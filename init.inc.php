1fc904267a8e5ea05362901da00771b2965f7206
webappkeeper <webappkeeper@gmail.com>
2018-09-01 21:03:23 +0800

    new

---
+++ init.inc.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC"-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml-strict.dtd">
+<html xmlns="http://www.w3.org//1999/xhtml">
+<head>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8"/>
+	<title>Web App Keeper</title>
+</head>
+<body>
+	<div id="center">
+		<div id="nav">
+			<a href="?page=home">Home</a>
+			<a href="?page=home">News</a>
+		</div>
+		<div id="wrap">
+			<?php
+			
+			?>
+		</div>
+	</div>
+</body>
+
+</html>
